a9f178c43a33f530dad0fd42ff8a764a59200dd5
Marcelo Wippel <marwippel@furb.br>
2018-09-10 21:40:56 -0300

    Commit da estrutura inicial do projeto.

---
+++ doar-vidas-backend/src/main/java/com/furb/br/doarvidas/DoarVidasApplication.java
@@ -0,0 +1,12 @@
+package com.furb.br.doarvidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoarVidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoarVidasApplication.class, args);
+	}
+}
