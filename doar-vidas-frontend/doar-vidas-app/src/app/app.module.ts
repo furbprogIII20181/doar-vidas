e70c3afb8fffb4c653dcdd0167497b36e1156261
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2018-09-13 20:14:49 -0300

    routes and toolbar and sidebar

--- doar-vidas-frontend/doar-vidas-app/src/app/app.module.ts
+++ doar-vidas-frontend/doar-vidas-app/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import {ROUTES} from './app.route';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatCardModule, MatToolbarModule, MatSidenavModule} from '@angular/material';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
