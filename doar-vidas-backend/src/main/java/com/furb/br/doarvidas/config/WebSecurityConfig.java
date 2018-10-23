319bf63d4b1fb7e75a34401aad821f9649153265
mawippel <mawippel2@hotmail.com>
2018-10-22 21:51:35 -0300

    Correções gerais da parte de login e integração com front

--- doar-vidas-backend/src/main/java/com/furb/br/doarvidas/config/WebSecurityConfig.java
+++ doar-vidas-backend/src/main/java/com/furb/br/doarvidas/config/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.furb.br.doarvidas.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Responsable for configure the Security of the application
@@ -32,16 +39,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .userDetailsService(customUserDetailsService)
             .passwordEncoder(passwordEncoder);    
     }
+    
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+      FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(corsConfigurationSource()));
+      bean.setOrder(SecurityProperties.DEFAULT_FILTER_ORDER);
+      return bean;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(true);
+      config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("*");
+      source.registerCorsConfiguration("/**", config);
+      return source;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
     	http
 	        .authorizeRequests()
-	            .antMatchers("/oauth/token").permitAll()
+	            .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
 	            .anyRequest().authenticated()
 	            .and()
 	        .httpBasic()
 	            .and()
+	        .cors().disable()
 	        .csrf().disable();
     }
     
