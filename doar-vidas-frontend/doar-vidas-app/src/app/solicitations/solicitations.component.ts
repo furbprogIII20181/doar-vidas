374b5ba7d0a02b5c21a23166bca0f1be263b2c05
mawippel <mawippel2@hotmail.com>
2018-10-25 21:55:51 -0300

    Criação da table de solicitations

--- doar-vidas-frontend/doar-vidas-app/src/app/solicitations/solicitations.component.ts
+++ doar-vidas-frontend/doar-vidas-app/src/app/solicitations/solicitations.component.ts
@@ -3,52 +3,68 @@ import { MatTableDataSource } from '@angular/material';
 import { Solicitation } from '../model/solicitation.model';
 
 @Component({
-  selector: 'app-solicitations',
-  templateUrl: './solicitations.component.html',
-  styleUrls: ['./solicitations.component.css']
+    selector: 'app-solicitations',
+    templateUrl: './solicitations.component.html',
+    styleUrls: ['./solicitations.component.css']
 })
 export class SolicitationsComponent implements OnInit {
 
-  exampleData = [
-      {
-          "id": 12,
-          "institution": {
-              "id": 8,
-              "name": "Hemosc",
-              "lastName": "Blumenau",
-              "city": "Blumenau",
-              "state": "SC",
-              "phone": "123123",
-              "user": {
-                  "id": 6,
-                  "email": "hemosc@hotmail.com",
-                  "password": "$2a$10$5p.Gkl5raFiM/vx7gJMJm.qzBXrCwqrzS3HsitCEwzlBDwJJYONI6",
-                  "roles": [
-                      {
-                          "id": 7,
-                          "name": "USER"
-                      }
-                  ]
-              },
-              "description": "aaa",
-              "cnpj": 123456789
-          },
-          "bloodType": "ONEG",
-          "quantity": 2.5
-      }
-  ];
+    exampleData = [
+        {
+            "id": 12,
+            "institution": {
+                "id": 8,
+                "name": "Hemosc",
+                "lastName": "Blumenau",
+                "city": "Blumenau",
+                "state": "SC",
+                "phone": "123123",
+                "user": {
+                    "id": 6,
+                    "email": "hemosc@hotmail.com",
+                    "password": "$2a$10$5p.Gkl5raFiM/vx7gJMJm.qzBXrCwqrzS3HsitCEwzlBDwJJYONI6",
+                    "roles": [
+                        {
+                            "id": 7,
+                            "name": "USER"
+                        }
+                    ]
+                },
+                "description": "aaa",
+                "cnpj": 123456789
+            },
+            "bloodType": "ONEG",
+            "quantity": 2.5
+        }
+    ];
 
-  solicitationsTable = new MatTableDataSource(this.exampleData);
+    solicitationsTable = new MatTableDataSource(this.exampleData);
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.solicitationsTable.filterPredicate = (data: any, filter: string) => {
+            var str = this.getStringObject(data);
+            return str.toLowerCase().trim().includes(filter.toLowerCase().trim());
+        }
+    }
 
-  displayedColumns: string[] = ['id', 'institution.name', 'institution.city', 'institution.state', 'bloodType']
+    getStringObject(data) {
+        let str = ''
+        Object.keys(data).forEach(e => {
+            if (typeof data[e] === 'object') {
+                str += this.getStringObject(data[e]);
+            } else {
+                str += data[e];
+            }
+        });
+        return str;
+    }
 
-  applyFilter(filterValue: string) {
-    this.solicitationsTable.filter = filterValue.trim().toLowerCase();
-  }
+    displayedColumns: string[] = ['id', 'institution.name', 'institution.city', 'institution.state', 'bloodType', 'actions']
+
+    applyFilter(filterValue: string) {
+        this.solicitationsTable.filter = filterValue.trim().toLowerCase();
+    }
 
 }
